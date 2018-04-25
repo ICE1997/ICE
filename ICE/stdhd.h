09a18cb6fec5cabb332610a9a1da2048427bb6a6
Mason <592092078@qq.com>
2018-04-25 20:18:35 +0800

    create testing

---
+++ ICE/stdhd.h
@@ -0,0 +1,12 @@
+//
+//  stdhd.h
+//  ICE
+//
+//  Created by mason on 2018/4/25.
+//  Copyright © 2018年 mason. All rights reserved.
+//常用头文件
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "time.h"
+#include <string.h>
